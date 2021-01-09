0241e80264e79891551f401d07db5199776f96e3
pwndream <73258312+pwndream@users.noreply.github.com>
2021-01-09 08:19:57 +0700

    Create find.py

---
+++ find.py
@@ -0,0 +1,19 @@
+import requests
+
+def find():
+    urlInput = input('Weblist: ')
+    url = open(urlInput, 'r')
+    print('\nContoh file: /wso.php, /up.php, /mini.php, /idx.php')
+    shellext = input('Nama File Shell: ')
+    print('\nContoh Title: WebOrbShell, Uploader By, Indoxploit Shell')
+    shelltitle = input('Title Shell: ')
+    for a in url.readlines():
+        urla = a.replace('\n', '')
+        print(' '+urla+shellext)
+        r = requests.get(urla+shellext)
+        if shelltitle in r.text:
+            print(' {} --> Shell Found [{}]'.format(urla, shellext))
+            saveshell = open('shell.txt', 'a')
+            saveshell.write(urla+shellext+'\n')
+
+find()
